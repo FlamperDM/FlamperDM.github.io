6cb278ba881f02b466fd5f46a4340de7675586f3
FlamperDM <flampeyeiri12@gmail.com>
2020-09-02 00:25:46 -0400

    Updates

---
+++ precache-manifest.a6bd1f34deb4ed3972cf5df94ef9dfed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89454be549fa5d48709a00c2d9cf2b2",
+    "url": "/FlamperDM.github.io/index.html"
+  },
+  {
+    "revision": "9a5c36edbe49d3da3e26",
+    "url": "/FlamperDM.github.io/static/css/main.8bf072d3.chunk.css"
+  },
+  {
+    "revision": "43b155b1382bd030f5aa",
+    "url": "/FlamperDM.github.io/static/js/2.eb97e1c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FlamperDM.github.io/static/js/2.eb97e1c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a5c36edbe49d3da3e26",
+    "url": "/FlamperDM.github.io/static/js/main.4f2bdfea.chunk.js"
+  },
+  {
+    "revision": "2821922454713741520c",
+    "url": "/FlamperDM.github.io/static/js/runtime-main.c57d004b.js"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/FlamperDM.github.io/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/FlamperDM.github.io/static/media/map-image.f40ed5b1.png"
+  }
+]);
